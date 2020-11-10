f014c9f65f4454acf2ced29e1dedc7aa642ba9e8
UmarUzdanov <umarhope@gmail.com>
2020-11-10 12:51:22 -0600

    Hello.java added new loop

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("How do we do this?");////
+        // are this people alive
+        for (int i =0; i<100; i ++){
+            System.err.print("Sykini deti ");
+        }
     }
 }
